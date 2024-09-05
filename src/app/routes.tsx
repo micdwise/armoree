58cae204927e56f5f5b4c7e67625970bb6cdc76a
Michael Wise <micwise@gmail.com>
2024-09-05 04:40:55 -0500

    Adding sidebar menu

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,29 @@
 import * as React from 'react';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, useLocation, RouteComponentProps } from 'react-router-dom';
 import { Ammunition } from '@app/Ammunition/Ammunition';
 
+export interface IAppRoute {
+  label?: string;
+  component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+  exact?: boolean;
+  path: string;
+  title: string;
+  routes?: undefined;
+}
+
+export interface IAppRouteGroup {
+  label: string;
+  routes: IAppRoute[];
+}
+
+export type AppRouteConfig = IAppRoute | IAppRouteGroup;
+
+const routes: AppRouteConfig[] = [
+  {
+    component: Ammunition;
+    exact: true,
+    label: 'Ammunition',
+    path: '/Ammunition',
+    title: 'Ammunition',
+  },
+];
