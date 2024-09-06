0486fb1674e07534dbe9c6b922a0c6a34de65527
Michael Wise <micwise@gmail.com>
2024-09-05 21:14:35 -0500

    Added Modal

--- src/app/Ammunition/Ammunition.tsx
+++ src/app/Ammunition/Ammunition.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 import { TableSelectable } from '@app/Ammunition/AmmunitionTable'
+import { ModalWithForm } from './AddAmmoForm';
 
 const Ammunition: React.FunctionComponent = () => (
   <PageSection hasBodyWrapper={false}>
     <Title headingLevel='h1' size='lg'>
       <TableSelectable />
+      <ModalWithForm />
     </Title>
   </PageSection>
 );
