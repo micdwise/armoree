814fc6c93d4dd7028277c78ee2eff2f88465a0d4
Michael Wise <micwise@gmail.com>
2024-09-30 16:19:40 -0500

    Make changes for CORS

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,19 @@
-import express  from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import ammunitionRouter from "./src/routes/ammunition";
 import firearmsRouter from "./src/routes/firearms";
-import cors from "cors"
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('*', cors());
+app.use(cors());
 
-app.use ((req, res, next) => {
+app.use((req, res, next) => {
   console.log("routing");
   next();
 });
@@ -21,7 +21,6 @@ app.use ((req, res, next) => {
 app.use("/api/v1/ammunition", ammunitionRouter);
 app.use("/api/v1/firearms", firearmsRouter);
 
-
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
