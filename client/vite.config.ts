b065dc2a9624623f9bc072006e7a173eba93e480
Michael Wise <micwise@gmail.com>
2024-10-08 10:52:49 -0500

    Added vite.config.ts and made additional config changes

---
+++ client/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      "@assets": path.resolve(
+        __dirname,
+        "node_modules/@patternfly/react-core/dist/styles/assets/*"
+      ),
+    },
+  },
+  plugins: [react()],
+});
