99067a480431036a84aff2c718e0517bec06c0d3
Michael Wise <micwise@gmail.com>
2025-12-08 18:52:17 -0600

    install TailwindCC and setup config files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   resolve: {
@@ -8,9 +9,9 @@ export default defineConfig({
       "@app": path.resolve(__dirname, "src/app"),
       "@assets": path.resolve(
         __dirname,
-        "node_modules/@patternfly/react-core/dist/styles/assets/*"
+        "node_modules/@patternfly/react-core/dist/styles/assets/*",
       ),
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
